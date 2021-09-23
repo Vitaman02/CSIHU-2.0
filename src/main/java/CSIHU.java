d93b7d15b106ba38e08e03cadf8d2f8cd0cb14d7
Vitaman02 <56309299+Vitaman02@users.noreply.github.com>
2021-09-23 15:50:07 +0300

    looking at example

--- src/main/java/CSIHU.java
+++ src/main/java/CSIHU.java
@@ -1,2 +1,18 @@
-public class CSIHU {
+import discord4j.core.DiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+
+
+public final class CSIHU {
+    public static void main(final String[] args) {
+        final String TOKEN = args[0];
+        final DiscordClient client = DiscordClient.create(TOKEN);
+
+        client.login().flatMapMany(gateway -> gateway.on(MessageCreateEvent.class))
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> "!ping".equals(message.getContent()))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("Pong!"))
+                .blockLast();
+    }
 }
